fa5f67e82e6df8b5e22afc0ca476d5ff90bc294c
Pedro Crespo <crespo@itis.ethz.ch>
2019-12-09 11:53:27 +0100

    Updated validation

--- tools/create-input.py
+++ tools/create-input.py
@@ -3,7 +3,9 @@ from pathlib import Path
 import json
 
 current_dir = Path(sys.argv[0] if __name__ == "__main__" else __file__).resolve().parent
-code_file = current_dir / "sample.py"
+validation_dir = (current_dir / "..").resolve()
+
+code_file = validation_dir / "sample" / "src" / "Example_PurePython_Postpro_Osparc.py"
 
 with open("input.json", "wt") as fh:
     json.dump({
