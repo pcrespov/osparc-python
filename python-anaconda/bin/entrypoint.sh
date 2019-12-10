3bd03906168cf7137beb92e56ceb38b635d74fea
Pedro Crespo <crespo@itis.ethz.ch>
2019-12-10 17:11:03 +0100

    Keeps scripts in anaconda

--- python-anaconda/bin/entrypoint.sh
+++ python-anaconda/bin/entrypoint.sh
@@ -1,1 +1,108 @@
-# TODO:
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+# This entrypoint script:
+#
+# - Executes *inside* of the container upon start as --user [default root]
+# - Notice that the container *starts* as --user [default root] but
+#   *runs* as non-root user [$SC_USER_NAME]
+#
+echo "Entrypoint for stage ${SC_BUILD_TARGET} ..."
+echo "  User    :`id $(whoami)`"
+echo "  Workdir :`pwd`"
+
+
+# expect input/output/log folders to be mounted
+stat $INPUT_FOLDER &> /dev/null || \
+        (echo "ERROR: You must mount '$INPUT_FOLDER' to deduce user and group ids" && exit 1)
+stat $OUTPUT_FOLDER &> /dev/null || \
+    (echo "ERROR: You must mount '$OUTPUT_FOLDER' to deduce user and group ids" && exit 1)
+stat $LOG_FOLDER &> /dev/null || \
+    (echo "ERROR: You must mount '$LOG_FOLDER' to deduce user and group ids" && exit 1)
+
+# NOTE: expects docker run ... -v /path/to/input/folder:$INPUT_FOLDER
+# check input/output/log folders are owned by the same user
+if [[ $(stat -c %u $INPUT_FOLDER) -ne $(stat -c %u $OUTPUT_FOLDER) ]]
+then
+    (echo "ERROR: '$INPUT_FOLDER' and '$OUTPUT_FOLDER' have different user id's. not allowed" && exit 1)
+elif [[ $(stat -c %u $INPUT_FOLDER) -ne $(stat -c %u $LOG_FOLDER) ]]
+then
+    (echo "ERROR: '$INPUT_FOLDER' and '$LOG_FOLDER' have different user id's. not allowed" && exit 1)
+fi
+
+# check input/output/log folders are owned by the same group
+if [[ $(stat -c %g $INPUT_FOLDER) -ne $(stat -c %g $OUTPUT_FOLDER) ]]
+then
+    (echo "ERROR: '$INPUT_FOLDER' and '$OUTPUT_FOLDER' have different group id's. not allowed" && exit 1)
+elif [[ $(stat -c %g $INPUT_FOLDER) -ne $(stat -c %g $LOG_FOLDER) ]]
+then
+    (echo "ERROR: '$INPUT_FOLDER' and '$LOG_FOLDER' have different group id's. not allowed" && exit 1)
+fi
+
+USERID=$(stat -c %u $INPUT_FOLDER)
+GROUPID=$(stat -c %g $INPUT_FOLDER)
+
+GROUPNAME=""
+if [[ $(getent group $GROUPID) ]]; then
+GROUPNAME=$(getent group ${GROUPID} | cut -d: -f1)
+fi
+
+if [[ $USERID -eq 0 ]]
+then
+    echo "Warning: Folder mounted owned by root user... adding $SC_USER_NAME to root..."
+    adduser $SC_USER_NAME root
+else
+    echo "Folder mounted owned by user $USERID:$GROUPID-'$GROUPNAME'..."
+    # take host's credentials in $SC_USER_NAME
+    if [[ -z "$GROUPNAME" ]]
+    then
+        echo "Creating new group ${SC_USER_NAME}grp"
+        GROUPNAME=${SC_USER_NAME}grp
+        addgroup --gid $GROUPID $GROUPNAME
+        # change group property of files already around
+        find / -path /proc -prune -group $SC_USER_ID -exec chgrp -h $GROUPNAME {} \;
+    else
+        echo "adding $SC_USER_NAME to group $GROUPNAME..."
+        adduser $SC_USER_NAME $GROUPNAME
+    fi
+
+    echo "changing $SC_USER_NAME $SC_USER_ID:$SC_USER_ID to $USERID:$GROUPID"
+    deluser $SC_USER_NAME &> /dev/null
+    if [[ "$SC_USER_NAME" == "$GROUPNAME" ]]
+    then
+        addgroup --gid $GROUPID $GROUPNAME
+    fi
+    adduser --disabled-password --gecos "" --uid $USERID --gid $GROUPID --shell /bin/bash $SC_USER_NAME --no-create-home
+    chown -R $SC_USER_NAME:$GROUPNAME /home/$SC_USER_NAME
+    # change user property of files already around
+    find / -path /proc -prune -user $SC_USER_ID -exec chown -h $SC_USER_NAME {} \;
+fi
+
+
+# Appends docker group if socket is mounted
+DOCKER_MOUNT=/var/run/docker.sock
+
+if [[ -e $DOCKER_MOUNT ]]
+then
+    GROUPID=$(stat -c %g $DOCKER_MOUNT)
+
+    GROUPNAME=scdocker
+
+    addgroup --gid $GROUPID $GROUPNAME &> /dev/null
+    if [[ $? -gt 0 ]]
+    then
+        # if group already exists in container, then reuse name
+        GROUPNAME=$(getent group ${GROUPID} | cut -d: -f1)
+    fi
+    adduser $SC_USER_NAME $GROUPNAME
+fi
+
+echo "Starting $@ ..."
+echo "  $SC_USER_NAME rights    :`id $SC_USER_NAME`"
+echo "  local dir :`ls -al`"
+echo "  input dir :`ls -al $INPUT_FOLDER`"
+echo "  output dir :`ls -al $OUTPUT_FOLDER`"
+echo "  log dir :`ls -al $LOG_FOLDER`"
+
+exec su --preserve-environment --command "export PATH=${PATH}:/home/$SC_USER_NAME/service.cli; $@" "$SC_USER_NAME"
